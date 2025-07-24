01a7416a7036f5bb03336615e45a02a7a3ecd490
BalanlaySC <balanlaysc@gmail.com>
2025-07-25 00:49:02 +0800

    feat: test Vite Project Setup

---
+++ week1-vite-exercise/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Hello, Vite + React!</h1>
+      <p>Vite environment setup successful!</p>
+      <p>You're ready for the next steps!</p>
+    </div>
+  );
+}
+
+export default App;
