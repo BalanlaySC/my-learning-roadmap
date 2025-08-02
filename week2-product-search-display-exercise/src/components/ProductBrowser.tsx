4ea56a6f9090c04fd6eda788ffda23796aed9029
BalanlaySC <balanlaysc@gmail.com>
2025-08-02 20:34:26 +0800

    feat(products): Implement search filtering and mock API fetch (only upon page load)

--- week2-product-search-display-exercise/src/components/ProductBrowser.tsx
+++ week2-product-search-display-exercise/src/components/ProductBrowser.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { Product } from '../data/types';
 import products from '../data/products'
 import ProductCard from '../components/ProductCard'
@@ -10,6 +10,33 @@ const ProductBrowser: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
   const [searchQuery, setSearchQuery] = useState<string>('');
 
+  useEffect(() => {
+    const fetchProducts = (): Promise<Product[]> => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(products);
+        }, 800); // 800ms delay
+      });
+    };
+
+    fetchProducts()
+      .then((data) => {
+        setProducts(data);
+        setLoading(l => l = false);
+      })
+      .catch(() => {
+        setError(e => e = "not found.");
+      });
+  }, []);
+
+  const handleSearch = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setSearchQuery(event.target.value)
+  };
+
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="p-4 m-4 bg-white rounded-lg shadow-md">
       <div className="flex items-center justify-between">
@@ -19,17 +46,26 @@ const ProductBrowser: React.FC = () => {
             id='prodSearchBar'
             type="text"
             placeholder="Search products..."
+            value={searchQuery}
+            onChange={handleSearch}
             className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+          {/* <button 
+            onClick={handleSearch}
+            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+          >
             Search
-          </button>
+          </button> */}
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {curProducts.map((prod) => (
-          <ProductCard key={prod.id} product={prod} />
-        ))}
+        {loading ? (
+          <p>Loading products...</p>
+        ) : (
+          filteredProducts.map((prod) => (
+            <ProductCard key={prod.id} product={prod} />
+          ))
+        )}
       </div>
     </div>
   );
